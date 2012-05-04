54d9a56a34ba102282b291102fa645c49d5ed24a
ron gokhle <cips173@gmail.com>
2012-05-04 10:46:33 +0530

    adding support for my pecan omgb&omfgb nightly

---
+++ omgb/pecan.js
@@ -0,0 +1,12 @@
+[
+{
+"device"="pecan",
+"version"="1.2.3",
+"date"="05-05",
+"name"="OMGB_pecan-1.2.3.zip",
+"url"="http://r2doesinc.bitsurge.net/omgb/heroc/OMGB_heroc-1.2.2.zip",
+"installable"="false",
+"description"="First release from cips for LG-P350."
+},
+ 
+]
---
+++ pecan.js
@@ -0,0 +1,12 @@
+[
+{
+"device"="pecan",
+"version"="1.3.1",
+"date"="05-05",
+"name"="OMFGB_pecan-1.3.1.zip",
+"url"="http://r2doesinc.bitsurge.net/nightlies/heroc/OMFGB_heroc-1.3.1_nightly-09-14.zip",
+"installable"="false",
+"description"="3rd release from cips for LG-P350."
+},
+ 
+]
